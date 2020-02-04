f52a93153cd4fdd3b5e585fbe9c3792f69f25ad8
Khusro Kidwai <khusrokidwai@gmail.com>
2020-02-04 18:12:03 -0500

    update

---
+++ build/precache-manifest.c35c9113b2e98dc10e75e626c3237219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af609c8fa1a9b1eb8958b968ad4f94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd516ad12f0a66045115",
+    "url": "/static/js/2.5e912b14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5e912b14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77f3a9216518cc9ee740",
+    "url": "/static/js/main.0b3d5737.chunk.js"
+  },
+  {
+    "revision": "5dba08e5dd7e6a1141cc",
+    "url": "/static/js/runtime-main.d4b0c7e6.js"
+  }
+]);
